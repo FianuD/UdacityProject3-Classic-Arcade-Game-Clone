644dc78e58725a2ddc4ff575d121b5b0e1eb5c15
FianuD <dovlofianu@gmail.com>
2018-09-11 02:48:54 +0000

    Feature: Added collision detection. Fixed some stray commas.

--- js/app.js
+++ js/app.js
@@ -8,25 +8,33 @@ var Enemy = function(x, y, speed) {
     // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
 };
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // Speed muliplied on x axis
     this.x += this.speed * dt;
-
     // Once enemies are off canvas, make them reappear with different speeds
     if (this.x > 510){
         this.x = 50;
         this.speed = 100 + Math.floor(Math.random() * 222);
     }
+    // Check for collision between player and enemy
+    if ((player.x < this.x + 80 && player.x + 80 > this.x) &&
+        (player.y < this.y + 60 && player.y + 60 > this.y)){
+            player.x = 202;
+            player.y = 405;
+        }
 };
+
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 // Player class
-var Player = function (x, y,){
+var Player = function (x, y){
     // Variables for the player to move along the x and y axis of the board
     this.x = x;
     this.y = y;
@@ -36,29 +44,31 @@ var Player = function (x, y,){
 
 Player.prototype.update = function(dt){
 };
+
 // Draw the player character on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.player), this.x, this.y);
 };
+
 // Control Player with arrow keys
 Player.prototype.handleInput = function(keyPress) {
-    // Left arrow key moves player on x-axis to the left by 102, keeps player within board
+    // Left arrow key moves player on x-axis to the left by 102, keeps player within bounds
     if (keyPress === 'left' && this.x > 0) {
         this.x -= 102;
     }
-    // Right arrow key moves player on x-axis to the right by 102, keeps player within board
+    // Right arrow key moves player on x-axis to the right by 102, keeps player within bounds
     if (keyPress === 'right' && this.x < 405) {
         this.x += 102;
     }
-    // Up arrow key moves player up on y-axis by 83, keeps player within board
+    // Up arrow key moves player up on y-axis by 83, keeps player within bounds
     if (keyPress === 'up' && this.y > 0) {
         this.y -= 83;
     }
-    // Down arrow key moves down player on x-axis by 83, keeps player within board
+    // Down arrow key moves down player on x-axis by 83, keeps player within bounds
     if (keyPress === 'down' && this.y < 405) {
         this.x += 83;
     }
-    // Reset player position upon reaching top top of page/water
+    // Reset player position upon reaching top of board/water
     if (this.y < 0) {
         setTimeout(() =>{
             this.x = 202;
@@ -66,6 +76,7 @@ Player.prototype.handleInput = function(keyPress) {
         }, 800);
     }
 };
+
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 // Location of the enemies on the y-axis
@@ -75,10 +86,10 @@ enemyLocation.forEach(function(locationY){
     enemy = new Enemy(0, locationY, (Math.floor(Math.random() * 222)));
     allEnemies.push(enemy);
 });
+
 // Place the player object in a variable called player
 // Have given player starting position in middle of board.
 var player = new Player(202, 405);
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
