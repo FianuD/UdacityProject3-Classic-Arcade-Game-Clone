6d339a3a9e7652e933ca7dd89bbbb380ebf55069
FianuD <dovlofianu@gmail.com>
2018-09-13 19:44:12 +0000

    Refactor: Changed from ES5 to ES6.

--- js/app.js
+++ js/app.js
@@ -1,89 +1,93 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // Used to determine the x and y axis and speed of enemy
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // Speed muliplied on x axis
-    this.x += this.speed * dt;
-    // Once enemies are off canvas, make them reappear with different speeds
-    if (this.x > 510){
-        this.x = 0;
-        this.speed = 80 + Math.floor(Math.random() * 200);
+class Enemy {
+    constructor(x, y, speed) {
+        // Variables applied to each of our instances go here,
+        // Used to determine the x and y axis and speed of enemy
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        // The image/sprite for our enemies
+        this.sprite = 'images/enemy-bug.png';
     }
-    // Check for collision between player and enemy
-    if ((player.x < this.x + 80 && player.x + 80 > this.x) &&
-        (player.y < this.y + 60 && player.y + 60 > this.y)){
-            // Sends player back to starting position
-            player.x = 202;
-            player.y = 405;
-        }
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // Speed muliplied on x axis
+        this.x += this.speed * dt;
+        // Once enemies are off canvas, make them reappear with different speeds
+        if (this.x > 510){
+            this.x = 0;
+            this.speed = 80 + Math.floor(Math.random() * 200);
+        }
+        // Check for collision between player and enemy
+        if ((player.x < this.x + 80 && player.x + 80 > this.x) &&
+            (player.y < this.y + 60 && player.y + 60 > this.y)){
+                // Sends player back to starting position
+                player.x = 202;
+                player.y = 405;
+            }
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Player class
-var Player = function (x, y){
-    // Variables for the player to move along the x and y axis of the board
-    this.x = x;
-    this.y = y;
-    // The image, sprite for our player
-    this.player = 'images/char-boy.png';
-};
+class Player {
+    constructor(x, y) {
+        // Variables for the player to move along the x and y axis of the board
+        this.x = x;
+        this.y = y;
+        // The image, sprite for our player
+        this.player = 'images/char-boy.png';
+    }
 
-Player.prototype.update = function(dt){
-};
+    update(dt) {
+    }
 
-// Draw the player character on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
-};
+    // Draw the player character on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+    }
 
-// Control Player with arrow keys
-Player.prototype.handleInput = function(keyPress) {
-    // Left arrow key moves player on x-axis to the left by 102, keeps player within bounds
-    if (keyPress === 'left' && this.x > 0) {
-        this.x -= 102;
+    // Control Player with arrow keys
+    handleInput(keyPress) {
+        // Left arrow key moves player on x-axis to the left by 102, keeps player within bounds
+        if (keyPress === 'left' && this.x > 0) {
+            this.x -= 102;
+        }
+        // Right arrow key moves player on x-axis to the right by 102, keeps player within bounds
+        if (keyPress === 'right' && this.x < 405) {
+            this.x += 102;
+        }
+        // Up arrow key moves player up on y-axis by 83, keeps player within bounds
+        if (keyPress === 'up' && this.y > 0) {
+            this.y -= 83;
+        }
+        // Down arrow key moves down player on x-axis by 83, keeps player within bounds
+        if (keyPress === 'down' && this.y < 405) {
+            this.y += 83;
+        }
+        // Reset player position upon reaching top of board/water
+        if (this.y < 0) {
+            setTimeout(() =>{
+                this.x = 202;
+                this.y = 405;
+            }, 500);
+        }
     }
-    // Right arrow key moves player on x-axis to the right by 102, keeps player within bounds
-    if (keyPress === 'right' && this.x < 405) {
-        this.x += 102;
-    }
-    // Up arrow key moves player up on y-axis by 83, keeps player within bounds
-    if (keyPress === 'up' && this.y > 0) {
-        this.y -= 83;
-    }
-    // Down arrow key moves down player on x-axis by 83, keeps player within bounds
-    if (keyPress === 'down' && this.y < 405) {
-        this.y += 83;
-    }
-    // Reset player position upon reaching top of board/water
-    if (this.y < 0) {
-        setTimeout(() =>{
-            this.x = 202;
-            this.y = 405;
-        }, 500);
-    }
-};
+}
 
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [];
+const allEnemies = [];
 // Location of the enemies on the y-axis
-var enemyLocation = [63, 147, 230];
+const enemyLocation = [63, 147, 230];
 // Initial enemy movement
-enemyLocation.forEach(function(locationY){
+enemyLocation.forEach(locationY => {
     enemy = new Enemy(0, locationY, (50 + Math.floor(Math.random() * 200)));
     allEnemies.push(enemy);
 });
@@ -94,7 +98,7 @@ var player = new Player(202, 405);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', e => {
     var allowedKeys = {
         37: 'left',
         38: 'up',
